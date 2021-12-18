61eba470f0c2f444b85c381b00fb48d5538582f2
matiasua <matiasncp7@gmail.com>
2021-12-18 17:06:53 -0300

    setting api documentation

--- icard/icard/urls.py
+++ icard/icard/urls.py
@@ -15,7 +15,24 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+
+schema_view = get_schema_view(
+   openapi.Info(
+      title="iCard - ApiDoc",
+      default_version='v1',
+      description="Documentacion de la api de iCard",
+      terms_of_service="https://www.github.com/matiasua",
+      contact=openapi.Contact(email="matiasncp7@gmail.com"),
+      license=openapi.License(name="BSD License"),
+   ),
+   public=True,
+)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('docs/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('redocs/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 ]
