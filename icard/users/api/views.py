b1d908c58c2f7d9753dcdcc80b6935c746630f38
matiasua <matiasncp7@gmail.com>
2021-12-19 19:48:01 -0300

    Permite actualizar la password y encriptarla ya creado el usuario

--- icard/users/api/views.py
+++ icard/users/api/views.py
@@ -21,3 +21,13 @@ class UserApiViewSet(ModelViewSet):
       # Obtiene la contraseña, la encripta
         request.data['password'] = make_password(request.data['password'])
         return super().create(request, *args, **kwargs)
+
+  # Nos permite actualizar la password y encriptarla ya creado el usuario.
+    def partial_update(self, request, *args, **kwargs):
+        password = request.data['password']
+
+        if password:
+            request.data['password'] = make_password(password)
+        else:
+          request.data['password'] = request.user.password
+        return super().update(request, *args, **kwargs)
